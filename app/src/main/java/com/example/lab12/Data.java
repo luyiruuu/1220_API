a53b1b510d07db2ab3ba5093efd757cc5b9654a9
luyiruuu <t111360125@ntut.org.tw>
2024-12-20 15:29:19 +0800

    first commit

---
+++ app/src/main/java/com/example/lab12/Data.java
@@ -0,0 +1,13 @@
+package com.example.lab12;
+
+public class Data {
+    Result result;
+    static class Result{
+        Results[] results;
+        static class Results{
+            String Station;
+            String Destination;
+        }
+    }
+
+}
